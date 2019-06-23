507e9b2e356b09e0b928e9f5dcc64b9f9c1af3bf
Keshav Damani <42195243+KeshavGitHub1998@users.noreply.github.com>
2019-06-23 18:53:34 +0530

    Add files via upload

---
+++ 2OpenFile.py
@@ -0,0 +1,22 @@
+#opening a file or knowing the path of the file
+from tkinter import *
+from tkinter import filedialog
+obj=Tk()
+obj.geometry("500x500+100+20")
+obj.title("opening the file")
+
+def OpenFile():
+    file1=filedialog.askopenfile()
+    labfile=Label(text=file1,font=10,bg='red').pack()
+    file2=file1.name
+    f=open(file2)
+    label2=Label(text=f.read(),font=10).pack()
+    return ("")
+    
+
+#making of the button
+ButtonOpen=Button(text="Click to choose the file",font=("arial",15,'italic'),command=OpenFile).pack()
+
+
+#mainloop
+obj.mainloop()
